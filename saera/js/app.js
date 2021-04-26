06b2ac89a7949fa1e0c3d16b10c2393d128a1655
ninefloor <less0805@gmail.com>
2021-04-26 16:04:40 +0900

    add lnb, nav js complete

--- saera/js/app.js
+++ saera/js/app.js
@@ -1,3 +1,26 @@
+$(document).ready(function(){
+  pcNav();
+})
+
+let gnb = $(".nav>ul>li");
+let lnb = $(".lnb");
+let wing = $(".wing");
+
+function pcNav(){
+  gnb.hover(
+    function(){
+      lnb.stop().fadeIn(600);
+      wing.stop().slideDown(300);
+    },
+    function(){
+      lnb.stop().fadeOut(200);
+      wing.stop().slideUp(400);
+    }
+  )
+}
+
+
+
 lightbox.option({
   resizeDuration: 200,
   wrapAround: false,
@@ -6,3 +29,5 @@ lightbox.option({
   showImageNumberLabel: false,
   alwaysShowNavOnTouchDevices: true
 });
+
+
