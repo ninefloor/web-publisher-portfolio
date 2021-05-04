bf65e2228e829cb9826c99c8f7ba80c2f48f008d
ninefloor <less0805@gmail.com>
2021-05-04 16:50:51 +0900

    link anker

--- saera/js/app.js
+++ saera/js/app.js
@@ -8,28 +8,7 @@ $(document).ready(function(){
       m_nav.css({left: "-"+100+"%"});
     }
   })
-  // slick 사용
-  $('.slide').slick({
-    slide: 'div', // 슬라이드 될 요소
-    infinite: true, // 무한 슬라이드
-    fade: true, // 페이드 전환 방식
-    speed: 300, // 전환 속도
-    arrows: false, // 화살표 숨김
-    dots: false, // 인디케이터
-    autoplay: true, // 자동 전환
-    autoplaySpeed: 4000, // 자동전환 주기
-    pauseOnHover: false, // 호버 시 자동전환 정지
-    mobileFirst: true, // 모바일향
-    respondTo: 'window',
-    responsive:[ // 반응형 옵션
-      {
-        breakpoint: 768, // 브레이크 포인트 : 768px 이상
-        settings:{
-          arrows: true // 화살표 표시
-        }
-      }
-    ]
-  })
+  
 })
 
 let gnb = $(".nav>ul>li");
@@ -76,16 +55,5 @@ function mNav(){
     )
   })
 }
-
-
-// lightbox 설정
-lightbox.option({
-  resizeDuration: 200,
-  wrapAround: false,
-  disableScrolling: true,
-  fitImagesInViewport: true,
-  showImageNumberLabel: false,
-  alwaysShowNavOnTouchDevices: true
-});
 
 
