652defa6ae7ffc701a3fc71fcd6d021dd4a6c40f
ninefloor <less0805@gmail.com>
2021-05-21 19:49:52 +0900

    햄버거 버튼 제작

--- wdicy/js/app.js
+++ wdicy/js/app.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
   title();
+  menuBtn();
 })
 
 function title(){
   $(".title").click(function(){
     $(this).stop().fadeOut(500);
     $("#wrap").fadeIn(500);
+  })
+}
+
+function menuBtn(){
+  $(".menu_btn").click(function(e){
+    e.preventDefault();
+    $(this).toggleClass("active");
   })
 }
