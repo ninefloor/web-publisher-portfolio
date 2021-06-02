790a856299a169e81ac85b979b21accebfe09aa0
ninefloor <less0805@gmail.com>
2021-06-02 16:45:17 +0900

    페럴렉스 이미지 효과 구현 완료

--- wdicy/js/app.js
+++ wdicy/js/app.js
@@ -2,7 +2,18 @@ $(document).ready(function(){
   title();
   menuBtn();
   track();
+  parallax();
 })
+
+// lightbox 설정
+lightbox.option({
+  resizeDuration: 200,
+  wrapAround: false,
+  disableScrolling: true,
+  fitImagesInViewport: true,
+  showImageNumberLabel: false,
+  alwaysShowNavOnTouchDevices: true
+});
 
 function title(){
   $(".title").click(function(){
@@ -34,6 +45,28 @@ function track(){
       $(this).find("h5").removeClass("active");
     } else if(listState==true && playState==false) {
       $(this).find("h5").addClass("active");
+    }
+  })
+}
+
+function parallax(){
+  let profile = $(".sec3");
+  let profileImg = $(".sec3>.contain>figure>div");
+  let track = $(".sec4");
+  let trackImg = $(".sec4>.contain>figure>div");
+  let video = $(".sec6");
+  let videoImg = $(".sec6>.contain>.mv>a");
+  
+  $(window).scroll(function(){
+    let windowScroll = $(this).scrollTop();
+    if(windowScroll >= profile.offset().top - $(window).height()/3){
+      profileImg.addClass("on");
+    }
+    if(windowScroll >= track.offset().top - $(window).height()/3){
+      trackImg.addClass("on");
+    }
+    if(windowScroll >= video.offset().top - $(window).height()/3){
+      videoImg.addClass("on");
     }
   })
 }
