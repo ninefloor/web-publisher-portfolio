c12038f2b97e377d35d3243e4ca460a9aa4514a8
ninefloor <less0805@gmail.com>
2021-07-01 20:02:52 +0900

    sec3 작업 진행

--- index/js/app.js
+++ index/js/app.js
@@ -1,3 +1,23 @@
-$(document).ready(function(){
-  $('#wrap').fullpage();
+$(function(){
+  $('#wrap').fullpage({
+    responsiveWidth: 769
+  });
+  abilityClickBox();
+  pageDescButton();
 })
+function abilityClickBox(){
+  $('.flip').click(function(){
+    $(this).toggleClass('active');
+  })
+}
+
+function pageDescButton(){
+  let button = $('.page_selector>button');
+  let pages = $('.page_contain>article');
+  button.on("click", function(){
+    let idx = $(this).index();
+    console.log(idx);
+    pages.removeClass('active');
+    pages.eq(idx).addClass('active');
+  })
+}
